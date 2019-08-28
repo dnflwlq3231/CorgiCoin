54bd48382adbc560a74aab35a0ccbd5d7b94cc60
corgi kim <dnflwlq3231@naver.com>
2019-08-28 11:24:58 +0900

    second commit

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,8 +1,8 @@
-var SampleToken = artifacts.require("./SampleToken.sol");
-const _name = "TestToken";
-const _symbol = "TTK";
+var CorgiCoin = artifacts.require("./CorgiCoin.sol");
+const _name = "CorgiCoin";
+const _symbol = "CGC";
 const _decimals = 18;
 const _total_supply = 1000000;
 module.exports = function(deployer) {
-    deployer.deploy(SampleToken, _name, _symbol, _decimals, _total_supply);
+    deployer.deploy(CorgiCoin, _name, _symbol, _decimals, _total_supply);
 };
